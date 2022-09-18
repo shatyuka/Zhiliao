0a28a1f2a47ea7e19fc9b4804c38d127e6121719
Shatyuka <958182453@qq.com>
2022-09-19 01:34:11 +0800

    Refine WebView inject

--- app/src/main/java/com/shatyuka/zhiliao/hooks/WebView.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/WebView.java
@@ -1,22 +1,16 @@
 package com.shatyuka.zhiliao.hooks;
 
-import android.webkit.ValueCallback;
+import android.graphics.Bitmap;
 
 import com.shatyuka.zhiliao.Helper;
-
-import java.lang.reflect.Method;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
 public class WebView implements IHook {
-    static Method onReceivedTitle;
-    static Method evaluateJavascript;
-
-    static int[] timeout_array = {100, 500, 1000};
-    static final String script_hide_water_mark = getScript("zhiliao_hide_water_mark", "let styleEle=document.createElement(\"style\");styleEle.innerHTML=\".App{background-image:none!important}\";document.body.append(styleEle);");
-    static final String script_hide_subscribe = getScript("zhiliao_hide_subscribe", "var subscribe=document.getElementsByClassName(\"Toolbar-functionButtons\")[0].lastChild;if(subscribe.firstChild.classList.contains(\"Avatar\")){subscribe.style.display=\"none\"};");
+    static final String script_hide_water_mark = "var styleWaterMark=document.createElement('style');styleWaterMark.innerHTML='.App{background-image:none!important}';document.body.append(styleWaterMark);";
+    static final String script_hide_subscribe = "var subscribe=document.getElementsByClassName('Toolbar-functionButtons')[0].lastChild;for(var i=0;i<subscribe.children.length;i++){if(subscribe.children[i].classList.contains('Avatar')){var styleSubscribe=document.createElement('style');styleSubscribe.innerHTML='.'+subscribe.className+'{display:none!important}';document.body.append(styleSubscribe);break}};";
 
     @Override
     public String getName() {
@@ -25,15 +19,6 @@ public class WebView implements IHook {
 
     @Override
     public void init(ClassLoader classLoader) throws Throwable {
-        Helper.findClass(classLoader, "com.zhihu.android.app.mercury.", 0, 2,
-                (Class<?> ZhihuWebChromeClientExt) -> {
-                    onReceivedTitle = ZhihuWebChromeClientExt.getMethod("onReceivedTitle", Helper.IZhihuWebView, String.class);
-                    return true;
-                });
-        if (onReceivedTitle == null)
-            throw new NoSuchMethodException("com.zhihu.android.app.mercury.ZhihuWebChromeClientExt.onReceivedTitle(IZhihuWebView, String)");
-
-        evaluateJavascript = Helper.IZhihuWebView.getMethod("a", String.class, ValueCallback.class);
     }
 
     @Override
@@ -47,30 +32,20 @@ public class WebView implements IHook {
             }
         });
 
-        XposedBridge.hookMethod(onReceivedTitle, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(Helper.WebViewClientWrapper, "onPageStarted", android.webkit.WebView.class, String.class, Bitmap.class, new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (Helper.prefs.getBoolean("switch_watermark", false)) {
-                    evaluateJavascript.invoke(param.args[0], script_hide_water_mark, null);
-                }
-                if (Helper.prefs.getBoolean("switch_subscribe", false)) {
-                    evaluateJavascript.invoke(param.args[0], script_hide_subscribe, null);
-                }
+            protected void afterHookedMethod(MethodHookParam param) {
+                android.webkit.WebView webview = (android.webkit.WebView) param.args[0];
+                boolean hideWaterMark = Helper.prefs.getBoolean("switch_watermark", false);
+                boolean hideSubscribe = Helper.prefs.getBoolean("switch_subscribe", false);
+                webview.evaluateJavascript(getScript(hideWaterMark, hideSubscribe), null);
             }
         });
 
         XposedHelpers.findAndHookMethod(Helper.WebViewClientWrapper, "onPageFinished", android.webkit.WebView.class, String.class, new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            protected void afterHookedMethod(MethodHookParam param) {
                 android.webkit.WebView webview = (android.webkit.WebView) param.args[0];
-                // also works after page reload
-                if (Helper.prefs.getBoolean("switch_watermark", false)) {
-                    webview.evaluateJavascript(script_hide_water_mark, null);
-                }
-                if (Helper.prefs.getBoolean("switch_subscribe", false)) {
-                    webview.evaluateJavascript(script_hide_subscribe, null);
-                }
-
                 String js = Helper.prefs.getString("edit_js", null);
                 if (js != null) {
                     webview.evaluateJavascript(js, null);
@@ -79,20 +54,16 @@ public class WebView implements IHook {
         });
     }
 
-    static String getScript(String name, String script) {
+    static String getScript(boolean hideWaterMark, boolean hideSubscribe) {
         StringBuilder sb = new StringBuilder();
-        sb.append("function ");
-        sb.append(name);
-        sb.append("(){");
-        sb.append(script);
-        sb.append("}");
-        for (int timeout : timeout_array) {
-            sb.append("setTimeout(");
-            sb.append(name);
-            sb.append(",");
-            sb.append(timeout);
-            sb.append(");");
+        sb.append("document.addEventListener('DOMContentLoaded',(event)=>{");
+        if (hideWaterMark) {
+            sb.append(script_hide_water_mark);
         }
+        if (hideSubscribe) {
+            sb.append(script_hide_subscribe);
+        }
+        sb.append("});");
         return sb.toString();
     }
 }
