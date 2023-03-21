a9b7296b4b04859c9254cd43cc9b3f5ac657baa0
shatyuka <958182453@qq.com>
2023-03-21 13:22:21 +0800

    In-page search words

--- app/src/main/java/com/shatyuka/zhiliao/hooks/AnswerAd.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/AnswerAd.java
@@ -6,6 +6,7 @@ import android.webkit.WebResourceResponse;
 import com.shatyuka.zhiliao.Helper;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -47,10 +48,34 @@ public class AnswerAd implements IHook {
                         || (Helper.prefs.getBoolean("switch_club", false) && segments.get(segments.size() - 1).equals("bind_club"))
                         || (Helper.prefs.getBoolean("switch_goods", false) && segments.get(segments.size() - 2).equals("goods"))
                         || (Helper.prefs.getBoolean("switch_article", false) && segments.get(segments.size() - 1).equals("recommendation"))
-                        || (Helper.prefs.getBoolean("switch_related", false) && segments.get(segments.size() - 3).equals("related_queries")))) {
+                        || (Helper.prefs.getBoolean("switch_related", false) && segments.get(segments.size() - 3).equals("related_queries")))
+                        || (Helper.prefs.getBoolean("switch_searchwords", false) && segments.get(segments.size() - 1).equals("entity_word"))) {
                     WebResourceResponse response = new WebResourceResponse("application/json", "UTF-8", new ByteArrayInputStream("{}".getBytes()));
                     response.setStatusCodeAndReasonPhrase(200, "OK");
                     param.setResult(response);
+                }
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                if (!Helper.prefs.getBoolean("switch_mainswitch", false))
+                    return;
+                WebResourceRequest request = (WebResourceRequest) param.args[1];
+                List<String> segments = request.getUrl().getPathSegments();
+                if (segments.size() > 2 && request.getMethod().equals("GET")) {
+                    if (Helper.prefs.getBoolean("switch_searchwords", false) && segments.get(0).equals("appview") && segments.get(segments.size() - 2).equals("answer")) {
+                        WebResourceResponse response = (WebResourceResponse) param.getResult();
+                        try {
+                            byte[] data = new byte[response.getData().available()];
+                            response.getData().read(data);
+                            String content = new String(data);
+                            content = content.replace("\"searchWords\"", "\"searchWords_bak\"");
+                            content = content.replace("\"search_words\"", "\"search_words_bak\"");
+                            response.setData(new ByteArrayInputStream(content.getBytes()));
+                            param.setResult(response);
+                        } catch (IOException ignored) {
+                        }
+                    }
                 }
             }
         });
