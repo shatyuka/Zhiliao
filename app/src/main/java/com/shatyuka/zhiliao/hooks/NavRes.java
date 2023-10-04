2a68d51ef0ac8c59f1c2b3b67314b310cb104323
Shatyuka <958182453@qq.com>
2023-10-04 22:01:42 +0800

    Fix NavRes

--- app/src/main/java/com/shatyuka/zhiliao/hooks/NavRes.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/NavRes.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Field;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
 public class NavRes implements IHook {
@@ -28,21 +29,38 @@ public class NavRes implements IHook {
         try {
             BottomNav = classLoader.loadClass("com.zhihu.android.bottomnav.a");
             if (BottomNav.getDeclaredField("a").getType() != boolean.class)
-                throw new Throwable("");
-        } catch (Throwable ignored) {
-            BottomNav = classLoader.loadClass("com.zhihu.android.bottomnav.b");
-            if (BottomNav.getDeclaredField("a").getType() != boolean.class)
                 throw new ClassNotFoundException("com.zhihu.android.bottomnav.BottomNav");
+        } catch (Throwable e) {
+            try {
+                BottomNav = classLoader.loadClass("com.zhihu.android.bottomnav.b");
+                if (BottomNav.getDeclaredField("a").getType() != boolean.class)
+                    throw new ClassNotFoundException("com.zhihu.android.bottomnav.BottomNav");
+            } catch (Throwable e2) {
+                BottomNav = classLoader.loadClass("com.zhihu.android.bottomnav.e");
+                if (BottomNav.getDeclaredField("a").getType() != boolean.class)
+                    throw new ClassNotFoundException("com.zhihu.android.bottomnav.BottomNav");
+            }
         }
         try {
             BottomNavBgViewExploreA = classLoader.loadClass("com.zhihu.android.bottomnav.core.explore.BottomNavBgViewExploreA");
-            BottomNavBgViewExploreA_right = BottomNavBgViewExploreA.getDeclaredField("b");
-            BottomNavBgViewExploreA_right.setAccessible(true);
-            BottomNavBgViewExploreA_center = BottomNavBgViewExploreA.getDeclaredField("c");
-            BottomNavBgViewExploreA_center.setAccessible(true);
+            try {
+                BottomNavBgViewExploreA_right = BottomNavBgViewExploreA.getDeclaredField("b");
+                BottomNavBgViewExploreA_right.setAccessible(true);
+                BottomNavBgViewExploreA_center = BottomNavBgViewExploreA.getDeclaredField("c");
+                BottomNavBgViewExploreA_center.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                BottomNavBgViewExploreA_right = BottomNavBgViewExploreA.getDeclaredField("l");
+                BottomNavBgViewExploreA_right.setAccessible(true);
+                BottomNavBgViewExploreA_center = BottomNavBgViewExploreA.getDeclaredField("m");
+                BottomNavBgViewExploreA_center.setAccessible(true);
+            }
         } catch (Throwable ignored) {
         }
-        FeedConfigManager = Helper.findClass(classLoader, "com.zhihu.android.", (Class<?> FeedConfigManager) -> FeedConfigManager.getMethod("a").getReturnType().getDeclaredField("a").getType().equals(String.class));
+        FeedConfigManager = Helper.findClass(classLoader, "com.zhihu.android.", 0, 2,
+                (Class<?> FeedConfigManager) -> {
+                    Class<?>[] types = FeedConfigManager.getConstructors()[0].getParameterTypes();
+                    return types.length == 6 && types[0] == String.class && types[1] == String.class && types[2] == long.class && types[3] == long.class && types[4] == types[5];
+                });
     }
 
     @Override
@@ -50,7 +68,12 @@ public class NavRes implements IHook {
         if (Helper.prefs.getBoolean("switch_mainswitch", false) && Helper.prefs.getBoolean("switch_navres", false)) {
             XposedHelpers.findAndHookMethod(BottomNav, "b", XC_MethodReplacement.returnConstant(null));
             if (FeedConfigManager != null) {
-                XposedHelpers.findAndHookMethod(FeedConfigManager, "a", XC_MethodReplacement.returnConstant(null));
+                XposedBridge.hookAllConstructors(FeedConfigManager, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.args[3] = 0;
+                    }
+                });
             }
         }
 
