b6955ded9c124aabf5f0213f7744cb5eb0a6aaa2
CLOUDERHEM <cloudac@qq.com>
2024-02-19 20:12:25 +0800

    feat: 关闭热榜自动刷新

--- app/src/main/java/com/shatyuka/zhiliao/hooks/AutoRefresh.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/AutoRefresh.java
@@ -10,18 +10,20 @@ import de.robv.android.xposed.XposedBridge;
 
 public class AutoRefresh implements IHook {
 
-    static Method shouldRefresh;
+    static Method feedAutoRefreshManager_shouldRefresh;
+
+    static Method feedHotRefreshAbConfig_shouldRefresh;
 
     @Override
     public String getName() {
-        return "关闭推荐页自动刷新";
+        return "关闭首页自动刷新";
     }
 
     @Override
     public void init(ClassLoader classLoader) throws Throwable {
         Class<?> feedAutoRefreshManager = classLoader.loadClass("com.zhihu.android.app.feed.util.j");
 
-        shouldRefresh = Arrays.stream(feedAutoRefreshManager.getDeclaredMethods())
+        feedAutoRefreshManager_shouldRefresh = Arrays.stream(feedAutoRefreshManager.getDeclaredMethods())
                 .filter(method -> method.getReturnType() == void.class)
                 .filter(method -> method.getParameterCount() == 4)
                 .filter(method -> {
@@ -29,16 +31,34 @@ public class AutoRefresh implements IHook {
                     return parameterTypes[0] == long.class && parameterTypes[1] == int.class;
                 }).findFirst().get();
 
+        Class<?> feedHotRefreshAbConfig = classLoader.loadClass("com.zhihu.android.app.feed.util.p");
+
+        feedHotRefreshAbConfig_shouldRefresh = Arrays.stream(feedHotRefreshAbConfig.getDeclaredMethods())
+                .filter(method -> method.getReturnType() == boolean.class)
+                .filter(method -> method.getParameterCount() == 1)
+                .filter(method -> method.getParameterTypes()[0] == long.class)
+                .findFirst().get();
+
     }
 
     @Override
     public void hook() throws Throwable {
-        XposedBridge.hookMethod(shouldRefresh, new XC_MethodHook() {
+        XposedBridge.hookMethod(feedAutoRefreshManager_shouldRefresh, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 if (Helper.prefs.getBoolean("switch_mainswitch", false)
                         && Helper.prefs.getBoolean("switch_autorefresh", true)) {
                     param.args[0] = 0;
+                }
+            }
+        });
+
+        XposedBridge.hookMethod(feedHotRefreshAbConfig_shouldRefresh, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (Helper.prefs.getBoolean("switch_mainswitch", false)
+                        && Helper.prefs.getBoolean("switch_autorefresh", true)) {
+                    param.setResult(false);
                 }
             }
         });
