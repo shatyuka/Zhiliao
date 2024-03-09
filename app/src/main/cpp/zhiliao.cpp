262b5779a24f1fc6fc7436abc302ed6982fdc868
Shatyuka <958182453@qq.com>
2024-03-09 19:00:21 +0800

    Upgrade dependencies

--- app/src/main/cpp/zhiliao.cpp
+++ app/src/main/cpp/zhiliao.cpp
@@ -20,11 +20,13 @@ FILE *fake_fopen(const char *filename, const char *mode) {
     return result;
 }
 
+#define PAGE_ALIGN(address, page_size) ((address) & (-(page_size)))
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_shatyuka_zhiliao_MainHook_initNative(JNIEnv *env, jobject) {
-    auto pagesize = sysconf(_SC_PAGE_SIZE);
-    auto addr = ((uintptr_t)fopen) & (-pagesize);
-    mprotect((void*)addr, pagesize, PROT_READ | PROT_WRITE | PROT_EXEC);
-    DobbyHook((void*)fopen, (void*)fake_fopen, (void**)&orig_fopen);
+    const auto page_size = sysconf(_SC_PAGE_SIZE);
+    const auto aligned = PAGE_ALIGN((uintptr_t)fopen, page_size);
+    mprotect((void*)aligned, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
+    DobbyHook((void*)fopen, (dobby_dummy_func_t)fake_fopen, (dobby_dummy_func_t*)&orig_fopen);
 }
