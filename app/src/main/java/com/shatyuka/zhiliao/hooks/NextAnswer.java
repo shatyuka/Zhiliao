69146403d77b91fa074198491d1343dd5108178f
shatyuka <shatyuka@qq.com>
2026-02-03 15:31:58 +0800

    Fix NextAnswer

--- app/src/main/java/com/shatyuka/zhiliao/hooks/NextAnswer.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/NextAnswer.java
@@ -42,7 +42,7 @@ public class NextAnswer implements IHook {
             initLayout = Helper.getMethodByParameterTypes(MixShortContainerFragment, 1, View.class);
 
             if (MixShortContainerFragment != null) {
-                String[] nextButton_names = new String[]{"t", "f", "e", "M", "L"};
+                String[] nextButton_names = new String[]{"t", "f", "e", "M", "L", "d"};
                 for (String name : nextButton_names) {
                     try {
                         Field field = MixShortContainerFragment.getDeclaredField(name);
